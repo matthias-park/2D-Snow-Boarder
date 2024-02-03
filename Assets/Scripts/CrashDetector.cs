81144f4146ad29d806ece8ddb4da7c480af0ffa3
Joons <matthiaspark0514@gmail.com>
2024-02-03 12:18:21 -0800

    Feat: Particle Effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crushEffect;
     void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crushEffect.Play();
             Invoke("ReloadScene", loadDelay);
         }
     }
